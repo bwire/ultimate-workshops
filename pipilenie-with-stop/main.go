87cc8fbca25f503268da9121823509c74541462e
yevheniy-gandzyuck <yevheniy.gandzyuck@theguarantors.com>
2025-05-22 11:59:42 +0200

    create flow reducer fn

--- pipilenie-with-stop/main.go
+++ pipilenie-with-stop/main.go
@@ -7,19 +7,22 @@ import (
 	"time"
 )
 
-type transformNumber struct {
+type flowStageFn func(int) int
+
+type flowStage struct {
 	n string
-	f func(int) int
+	f flowStageFn
+}
+
+func createStage(n string, f flowStageFn) flowStage {
+	return flowStage{n: n, f: f}
 }
 
 func generate(ctx context.Context, numElements int) <-chan int {
 	numStream := make(chan int)
 
 	go func() {
-		defer func() {
-			fmt.Println("generator stopped by timeout")
-			close(numStream)
-		}()
+		defer close(numStream)
 
 		for i := 1; i <= numElements; i++ {
 			select {
@@ -33,12 +36,12 @@ func generate(ctx context.Context, numElements int) <-chan int {
 	return numStream
 }
 
-func stage(ctx context.Context, input <-chan int, tn transformNumber) <-chan int {
+func stage(ctx context.Context, input <-chan int, fs flowStage) <-chan int {
 	output := make(chan int)
 
 	go func() {
 		defer func() {
-			fmt.Printf("stage '%s' stopped by timeout\n", tn.n)
+			fmt.Printf("stage '%s' stopped by timeout\n", fs.n)
 			close(output)
 		}()
 
@@ -53,7 +56,7 @@ func stage(ctx context.Context, input <-chan int, tn transformNumber) <-chan int
 				select {
 				case <-ctx.Done():
 					return
-				case output <- tn.f(v):
+				case output <- fs.f(v):
 					time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 				}
 			}
@@ -63,20 +66,32 @@ func stage(ctx context.Context, input <-chan int, tn transformNumber) <-chan int
 	return output
 }
 
+func processFlow(ctx context.Context, input <-chan int, stages ...flowStage) <-chan int {
+	ic := input
+	for _, s := range stages {
+		ic = stage(ctx, ic, s)
+	}
+	return ic
+}
+
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
 	num := 100
 
-	inputSeq := generate(ctx, num)
-	mulBy2 := stage(ctx, inputSeq, transformNumber{f: func(v int) int { return v * 2 }, n: "multiply by 2"})
-	plus1 := stage(ctx, mulBy2, transformNumber{f: func(v int) int { return v + 1 }, n: "add 1"})
-	valMulVal := stage(ctx, plus1, transformNumber{f: func(v int) int { return v * v }, n: "multiply value by value"})
+	flow := processFlow(
+		ctx,
+		generate(ctx, num),
+		createStage("multiply by 2", func(v int) int { return v * 2 }),
+		createStage("add 1", func(v int) int { return v + 1 }),
+		createStage("multiply value by value", func(v int) int { return v * v }),
+	)
 
-	for v := range valMulVal {
+	for v := range flow {
 		fmt.Println(v)
 	}
 
 	time.Sleep(200 * time.Millisecond)
+	fmt.Println("flow finished")
 }
