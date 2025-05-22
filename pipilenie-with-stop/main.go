0016057bd463fb27649db7842dc32b0fdd6ec5cc
yevheniy-gandzyuck <yevheniy.gandzyuck@theguarantors.com>
2025-05-22 10:21:03 +0200

    add pipeline-with-stop - extract generator intoa separate fn

--- pipilenie-with-stop/main.go
+++ pipilenie-with-stop/main.go
@@ -7,33 +7,33 @@ import (
 	"time"
 )
 
-func main() {
-	inputFn := func(ctx context.Context, numElements int) <-chan int {
-		numStream := make(chan int)
+func generate(ctx context.Context, numElements int) <-chan int {
+	numStream := make(chan int)
 
-		go func() {
-			defer close(numStream)
+	go func() {
+		defer close(numStream)
 
-			for i := 1; i <= numElements; i++ {
-				select {
-				case <-ctx.Done():
-					fmt.Println("input is closed by timeout")
-					return
-				case numStream <- i:
-				}
+		for i := 1; i <= numElements; i++ {
+			select {
+			case <-ctx.Done():
+				fmt.Println("input is closed by timeout")
+				return
+			case numStream <- i:
 				time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 			}
-		}()
+		}
+	}()
 
-		return numStream
-	}
+	return numStream
+}
 
+func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
 	defer cancel()
 
 	num := 100
 
-	for v := range inputFn(ctx, num) {
+	for v := range generate(ctx, num) {
 		fmt.Println(v)
 	}
 }
